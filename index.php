2743748dc988a6c46ba4017e28e529639194539c
Checo03 <Valdivia_sergio@outlook.com>
2023-11-26 14:05:31 -0600

    Estructura de La Pagina

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="shortcut icon" href="Media/Img/Favicon/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="Estilos/CabeceraEstilos.css">
+    <link rel="stylesheet" href="Estilos/InicioEstilos.css">
 </head>
 <body>
+    <header>
 
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
 </body>
 </html>
