dac4d3d378849b88deeb0ad546f07be873c6807d
DiegoPonce28 <diegodiegopa83@gmail.com>
2023-11-28 12:53:07 -0600

    Creacion acerca_de.php

---
+++ acerca_de.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de Nosotros</title>
+    <link rel="shortcut icon" href="Media/favicon.png" type="image/x-icon">
+    <script src="https://kit.fontawesome.com/0fe4f3447c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Estilos/style.css">
+</head>
+<body>
+    <header class="Cabecera-Header">
+        <div class="Container-Cabecera Nav-Container">
+            <img src="Media/img1.png" alt="LOGO" style="width: 150px;  height: 150px;">
+            <nav class="Cabecera-Nav Cabecera-Contenido">
+                <ul>
+                     <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Categorias</a></li>
+                    <li><a href="#">Conocenos</a></li>
+                    <li><a href="acerca_de.php">Acerca De</a></li>
+                </ul>
+            </nav>
+            <nav class="Cabecera-Nav Cabecera-Sesion">
+                <ul>
+                    <li><a href="#">Registrate</a></li>
+                    <li><a href="#">Inciar Sesion</a></li>
+                </ul>
+            </nav>
+            <div class="Cabecera-BarraBusqueda">
+                <form action="">
+                    <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+                    <input type="text" placeholder="¿Qué Esta Buscando?">
+                </form>
+            </div>
+            <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl" style="color: #ffffff;"></i></a>
+        </div>
+    </header>
+
+    <div class="content-container">
+        <section>
+            <h2><i class="fa-solid fa-headphones fa-bounce"></i> ¿QUIÉNES SOMOS?</h2>
+            <br>
+            <p><b>"ES UN PROYECTO ESCOLAR"</b><br>Somos un equipo apasionado que ha convertido la fascinación por el sonido en un proyecto de vida. En nuestro rincón digital, nos dedicamos a compartir experiencias auditivas excepcionales. Desde la primera nota hasta el último acorde, estamos aquí para guiarte en un viaje sonoro único.
+                Nos definimos por nuestra dedicación a la calidad, la innovación y la conexión con nuestra comunidad sonora.</p>
+        </section>
+    </div>
+    <br><br><br><br><br><br>
+    <div class="content-container">
+        <section>
+            <h2><i class="fa-solid fa-headphones fa-bounce"></i> NUESTRO EQUIPO</h2>
+            <br>
+            <p>En el corazón de nuestra tienda en línea de audífonos, existe un equipo apasionado y comprometido que trabaja incansablemente para brindarte una experiencia auditiva excepcional. Cada miembro de nuestro equipo comparte una conexión profunda con el mundo del sonido, fusionando conocimientos técnicos con una pasión innata para ofrecer lo mejor en tecnología de audio.</p>
+        </section>
+                <div class="slider">
+                    <span style="--i:2"><img src="Media/img2.png" alt="img2"></span>
+                    <span style="--i:3"><img src="Media/img3.png" alt="img3"></span>
+                    <span style="--i:4"><img src="Media/img4.png" alt="img4"></span>
+                    <span style="--i:5"><img src="Media/img5.png" alt="img5"></span>
+                    <span style="--i:6"><img src="Media/img6.png" alt="img6"></span>
+                    <span style="--i:7"><img src="Media/img7.png" alt="img7"></span>
+                    <span style="--i:8"><img src="Media/img8.png" alt="img8"></span>
+                    <span style="--i:9"><img src="Media/img9.png" alt="img9"></span>
+                </div>
+    </div>
+    <br><br><br><br><br><br><br><br>
+
+    <div class="TARJETA">
+        <div class="tarjetas">
+          <div style="background-image: url(Media/musica2.jpg);" class="tarjetas__item">
+            <h2 class="tarjetas__titulo">MISIÓN</h2>
+            <div class="tarjetas__info">
+              <p id="PT">Proporcionar a nuestros clientes una experiencia auditiva excepcional a través de una cuidadosa selección de audífonos de alta calidad, ofreciendo comodidad, estilo y rendimiento superior. Nos esforzamos por ser líderes en la industria de audio, brindando soluciones innovadoras y satisfaciendo las necesidades de nuestros clientes de manera integral.</p>
+            </div>
+          </div>
+          <div style="background-image: url(Media/musica3.jpg);" class="tarjetas__item">
+            <h2 class="tarjetas__titulo">VISIÓN</h2>
+            <div class="tarjetas__info">
+              <p id="PT">Ser reconocidos como la tienda en línea de referencia para audífonos, destacando por nuestra excelencia en servicio al cliente, variedad de productos de vanguardia y un compromiso constante con la satisfacción del usuario. Buscamos ser pioneros en la introducción de nuevas tecnologías y tendencias en el mercado de audio, elevando constantemente los estándares de calidad y brindando experiencias auditivas inigualables.</p>
+            </div>
+          </div>
+          <div style="background-image: url(Media/musica.jpg);" class="tarjetas__item">
+            <h2 class="tarjetas__titulo">OBJETIVO</h2>
+            <div class="tarjetas__info">
+              <p id="PT">Ofrecer una amplia gama de audífonos de marcas líderes, adaptados a diversas preferencias y necesidades de los clientes. Buscamos alcanzar y mantener una posición destacada en el mercado, incrementando la participación de clientes satisfechos y expandiendo nuestra presencia online. Además, nos esforzamos por garantizar una experiencia de compra fácil y segura, respaldada por un servicio de atención al cliente excepcional.</p>
+            </div>
+          </div>
+        </div>
+    </div>
+        <br><br><br><br><br>
+        <div class="content-container">
+            <section>
+                <h2><i class="fa-solid fa-headphones fa-bounce"></i> VALORES DE LA EMPRESA</h2>
+                <br>
+                <p>Los valores que una empresa reflejan su ética, cultura y principios fundamentales. Y para nosotros, es muy importante que nuestros clientes se sientan cómodos e incluidos con nuestros productos, reflejando la importancia que son ellos para nosotros.</p>
+            </section>
+        </div>
+        <br>
+        <ol id="lista5">
+            <li><b>Calidad:</b> Compromiso con la excelencia y la entrega de productos de alta calidad que satisfan las expectativas y necesidades de los clientes.</li>
+            <li><b>Innovación:</b> Buscar constantemente nuevas tecnologías y tendencias en la industria del audio para ofrecer productos de vanguardia.</li>
+            <li><b>Integridad:</b> Operar con honestidad, transparencia y ética en todas las interacciones comerciales y relaciones con los clientes.</li>
+            <li><b>Inclusividad:</b> Fomentar un ambiente inclusivo que celebre la diversidad, tanto en el equipo interno como entre los clientes.</li> 
+            <li><b>Sostenibilidad:</b> Compromiso con prácticas comerciales sociales y ambientalmente responsables, minimizando el impacto ambiental y contribuyendo positivamente a la comunidad.</li>
+        </ol>
+    <br><br><br><br>
+    <div style="border-radius: 50%; width: 50%;" class="content-container">
+        <center><section>
+            <h2>CONTÁCTANOS PARA MAYOR INFORMACIÓN</h2>
+            <br>
+            <center><a href="#" class="button">CONTÁCTANOS </a> <i style="font-size: x-large;" class="fa-solid fa-arrow-pointer fa-beat"></i></center>
+        </section></center>
+    </div>
+    <br><br><br><br>
+
+    <footer>
+        <h1 style="color: red;">AQUI VA EL FOOTER</h1>
+    </footer>
+</body>
+</html>
