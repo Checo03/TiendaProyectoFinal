69ee984b7b4a22c0eaa75a52f4d0b1997ef25afd
Checo03 <Valdivia_sergio@outlook.com>
2023-11-28 00:17:21 -0600

    Estructura Terminada

--- index.php
+++ index.php
@@ -7,91 +7,157 @@
     <link rel="shortcut icon" href="Media/Img/Favicon/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="Estilos/CabeceraEstilos.css">
     <link rel="stylesheet" href="Estilos/InicioEstilos.css">
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css'>
+    <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js'></script>
+ 
+    <script src="https://kit.fontawesome.com/91b95836a0.js" crossorigin="anonymous"></script>
 
-    <script src="https://kit.fontawesome.com/91b95836a0.js" crossorigin="anonymous"></script>  
-    
 </head>
 <body>
-    <header class="Cabecera-Header">
-        <div class="Container-Cabecera Nav-Container">
-            <img src="Media/Img/logo.png" alt="LOGO" style="width: 150px;  height: 150px;">
-            <nav class="Cabecera-Nav Cabecera-Contenido">
-                <ul>
-                     <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Categorias</a></li>
-                    <li><a href="#">Conocenos</a></li>
-                    <li><a href="#">Acerca De</a></li>
-                </ul>
-            </nav>
-            <nav class="Cabecera-Nav Cabecera-Sesion">
-                <ul>
-                    <li><a href="#">Registrate</a></li>
-                    <li><a href="#">Inciar Sesion</a></li>
-                </ul>
-            </nav>
-            <div class="Cabecera-BarraBusqueda">
-                <form action="">
-                    <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
-                    <input type="text" placeholder="¿Qué Esta Buscando?">
-                </form>
+    <div class="Container-Inicio">
+        <header class="Cabecera-Header">
+            <div class="Container-Cabecera Nav-Container">
+                <img src="Media/Img/logo.png" alt="LOGO" style="width: 150px;  height: 150px;">
+                <nav class="Cabecera-Nav Cabecera-Contenido">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Categorias</a></li>
+                        <li><a href="#">Conocenos</a></li>
+                        <li><a href="#">Acerca De</a></li>
+                    </ul>
+                </nav>
+                <nav class="Cabecera-Nav Cabecera-Sesion">
+                    <ul>
+                        <li><a href="#">Registrate</a></li>
+                        <li><a href="#">Inciar Sesion</a></li>
+                    </ul>
+                </nav>
+                <div class="Cabecera-BarraBusqueda">
+                    <form action="">
+                        <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+                        <input type="text" placeholder="¿Qué Esta Buscando?">
+                    </form>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl" style="color: #ffffff;"></i></a>
             </div>
-            <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl" style="color: #ffffff;"></i></a>
-        </div>
-    </header>
-    <main>
-        <section>
-            <div class="Carrusel">
-                <ul>
-                    <li class="Carrusel-Item">
-                        <img src="Media/Img/Carusel/img1.jpg">
-                        <div class="Carrusel-Content">
-                            <h2>Conecta con tu Música Desconecta del Mundo</h2>
-                            <p>Siente La Calidad En Cada Nota</p>
-                            <button>SEE MORE</button>
+        </header>
+        <main>
+            <div class="Img-Banner">
+                <div class="Container-Img-Banner">
+                    <img src="Media/Img/Inicio/Banner.jpg">
+                    <div class="Banner-Text">
+                        <h1>Revolt Sound Studios</h1>
+                        <p>Siente La Calidad En Cada Nota</p>
+                        <br><br>
+                        <button>SEE MORE</button>
+                    </div>
+                </div>
+            </div> 
+            
+            <div class="Container-Categorias">
+                <div class="Categorias-Title">
+                    <h1>CATEGORIAS</h1>
+                </div>
+                <div class="Categorias">
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/Diadema.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Diademas</div>
+
+                        </a>
+                    </div>
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/Earbuds.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">EarBuds</div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="Container-Ofertas">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7 text-center">
+                            <div class="section-title">
+                                <h2>Ultimas Ofertas</h2>
+                                <p>Aun no se acaba el buen fin aprovecha las ofertas que te da la familia Revolt</p>
+                            </div>
                         </div>
-                    </li>
-                    <li class="Carrusel-Item">
-                        <img src="Media/Img/Carusel/img2.jpg">
-                        <div class="Carrusel-Content">
-                            <h2>Sintoniza tu Mundo</h2>
-                            <p>Siente La Calidad En Cada Nota</p>
-                            <button>SEE MORE</button>
-                        </div>    
-                    </li>
-                    <li class="Carrusel-Item">
-                        <img src="Media/Img/Carusel/img3.jpg">
-                        <div class="Carrusel-Content">
-                            <h2>Decibeles que Enamoran</h2>
-                            <p>Siente La Calidad En Cada Nota</p>
-                            <button>SEE MORE</button>
-                        </div>  
-                    </li>
-                    <li class="Carrusel-Item">
-                        <img src="Media/Img/Carusel/img4.jpg">
-                        <div class="Carrusel-Content">
-                            <h2>Despierta tus Sentido</h2>
-                            <p>Siente La Calidad En Cada Nota</p>
-                            <button>SEE MORE</button>
-                        </div>     
-                    </li>
-                    <li class="Carrusel-Item">
-                        <img src="Media/Img/Carusel/img5.jpg">
-                        <div class="Carrusel-Content">
-                            <h2>Vibra con tu Estilo</h2>
-                            <p>Siente La Calidad En Cada Nota</p>
-                            <button>SEE MORE</button>
-                        </div>  
-                    </li>
-                </ul>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="Oferta">
+                                <div class="Img-Oferta">
+                                    <div class="date">04 FEB</div>
+                                    <a href="#">
+                                        <img src="Media/Img/Inicio/oferta1.jpg" title="" alt="">
+                                    </a>
+                                </div>
+                                <div class="blog-info">
+                                    <h5 ><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                    <div class="btn-bar">
+                                        <a href="#" class="px-btn-arrow">
+                                            <span>Ver Oferta</span>
+                                            <i class="arrow"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="Oferta">
+                                <div class="Img-Oferta">
+                                    <div class="date">04 FEB</div>
+                                    <a href="#">
+                                        <img src="Media/Img/Inicio/oferta2.jpg" title="" alt="">
+                                    </a>
+                                </div>
+                                <div class="blog-info">
+                                    <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                    <div class="btn-bar">
+                                        <a href="#" class="px-btn-arrow">
+                                            <span>Ver Oferta</span>
+                                            <i class="arrow"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="Oferta">
+                                <div class="Img-Oferta">
+                                    <div class="date">04 FEB</div>
+                                    <a href="#">
+                                        <img src="Media/Img/Inicio/oferta3.jpg" title="" alt="">
+                                    </a>
+                                </div>
+                                <div class="blog-info">
+                                    <h5><a href="#">Prevent 75% of visitors from google analytics</a></h5>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.</p>
+                                    <div class="btn-bar">
+                                        <a href="#" class="px-btn-arrow">
+                                            <span>Ver Oferta</span>
+                                            <i class="arrow"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </section>  
-        
+            
 
-    </main>
-    <footer>
+        </main>
+        <footer>
 
-    </footer>
+        </footer>
+    </div>
+    
 
-    <script src="Scripts/Carrusel.js"></script>
+ 
 </body>
 </html>
