dce384e540012df19cc9d6f59662b942c4f6a66e
DiegoPonce28 <diegodiegopa83@gmail.com>
2023-12-02 17:27:00 -0600

    Actualizacion de la pagina de Inicio

--- index.php
+++ index.php
@@ -5,71 +5,155 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Revolt Studio Sounds</title>
     <link rel="shortcut icon" href="Media/Img/Favicon/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="Estilos/CabeceraEstilos.css">
-    <link rel="stylesheet" href="Estilos/InicioEstilos.css">
+    <link rel="stylesheet" href="Estilos/InicioEstiloss.css">
+    <link rel="stylesheet" href="Estilos/style.css">
     <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css'>
     <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js'></script>
- 
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/91b95836a0.js" crossorigin="anonymous"></script>
 
 </head>
 <body>
     <div class="Container-Inicio">
-        <header class="Cabecera-Header">
-            <div class="Container-Cabecera Nav-Container">
-                <img src="Media/Img/logo.png" alt="LOGO" style="width: 150px;  height: 150px;">
-                <nav class="Cabecera-Nav Cabecera-Contenido">
-                    <ul>
-                        <li><a href="#">Inicio</a></li>
-                        <li><a href="#">Categorias</a></li>
-                        <li><a href="#">Conocenos</a></li>
-                        <li><a href="#">Acerca De</a></li>
-                    </ul>
-                </nav>
-                <nav class="Cabecera-Nav Cabecera-Sesion">
-                    <ul>
-                        <li><a href="#">Registrate</a></li>
-                        <li><a href="#">Inciar Sesion</a></li>
-                    </ul>
-                </nav>
-                <div class="Cabecera-BarraBusqueda">
-                    <form action="">
-                        <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
-                        <input type="text" placeholder="¿Qué Esta Buscando?">
-                    </form>
+        <header class="header">
+            <nav class="navbar navbar-expand-lg fixed-top py-2">
+                <div class="containerN">
+                    <a href="#" class="navbar-brand"><img src="Media/Img/logo_final.png" alt="LOGO" style="width: 70px;  height: 60px;"></a>
+                    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler navbar-toggler-right"><i class="fa fa-bars"></i></button>
+                    
+                    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+                            <li class="nav-item active" style="margin-right: 10px; margin-left: 30px;"><a href="#" class="nav-link text-uppercase font-weight-bold">Inicio <span class="sr-only">(current)</span></a></li>
+                            <li class="nav-item dropdown" style="margin-right: 10px;">
+                                <a href="#" class="nav-link dropdown-toggle text-uppercase font-weight-bold" data-bs-toggle="dropdown">Categorias</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="http://www.google.com">Diademas</a>
+                                    <a class="dropdown-item" href="http://www.bing.com">EarBuds</a>
+                                    <a class="dropdown-item" href="http://www.yahoo.com">Por marcas</a>
+                                </div>
+                            </li>
+                            <li class="nav-item" style="margin-right: 10px;"><a href="#" class="nav-link text-uppercase font-weight-bold">Conocenos</a></li>
+                            <li class="nav-item" style="margin-right: 10px;"><a href="#" class="nav-link text-uppercase font-weight-bold">Acerca De</a></li>
+                            <form style="margin-left: 160px;" class="d-flex" action="">
+                                <input class="form-control mr-2" type="search" placeholder="¿Qué estas buscando?" aria-label="¿Qué estas buscando?">
+                                <button class="btn btn-outline-success" type="submit">Buscar</button>
+                            </form>
+                            <li class="nav-item" style="margin-right: 5px; margin-left: 30px;"><a href="#" class="btn btn-outline-primary">Login</a></li>
+                            <li class="nav-item" style="margin-right: 5px;"><a href="#" class="btn btn-outline-primary">Registrarse</a></li>
+                            <li class="nav-item" style="margin-left: 20px;"><a href="#" class="nav-link"><i class="fa-solid fa-cart-shopping" style="color: #ffffff; font-size: 24px;"></i></a></li>       
+                        </ul>
+                    </div>
                 </div>
-                <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl" style="color: #ffffff;"></i></a>
-            </div>
-        </header>
+            </nav>
+        </header>      
         <main>
             <div class="Img-Banner">
                 <div class="Container-Img-Banner">
-                    <img src="Media/Img/Inicio/Banner.jpg">
+                    <img src="Media/Img/Inicio/Banner1.jpg">
                     <div class="Banner-Text">
                         <h1>Revolt Sound Studios</h1>
-                        <p>Siente La Calidad En Cada Nota</p>
+                        <p>Siente la calidad en cada nota</p>
                         <br><br>
-                        <button>SEE MORE</button>
+                        <button>VER MAS</button>
                     </div>
                 </div>
             </div> 
             
-            <div class="Container-Categorias">
-                <div class="Categorias-Title">
-                    <h1>CATEGORIAS</h1>
+            <section>
+                <div class="Categorias-Title" style="margin-top: 50px;">
+                    <h2>Conoce nuestros nuevos productos...</h2>
+                    <p style="font-size: 20px; font-style: italic;" class="text-center">Productos 100% oiginales de la mejor calidad, ganando la confianza de nuestros clientes</p>
                 </div>
-                <div class="Categorias">
+                <div class="accordian">
+                    <ul>
+                        <li>
+                            <div class="image_title">
+                                <a href="#">Audífonos Gamer Inalámbricos - Blancos</a>
+                            </div>
+                            <a href="#">
+                                <img style="height: 400px; width: 685px;" src="Media/Img/Contactanos/promos/audif.jpg"/>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="image_title">
+                                <a href="#">Audífonos Gamer Inalámbricos - Morados</a>
+                            </div>
+                            <a href="#">
+                                <img style="height: 400px; width: 890px;" src="Media/Img/Contactanos/promos/audiff2.jpeg">
+                            </a>
+                        </li>
+                        <li>
+                            <div class="image_title">
+                                <a href="#">Audífonos Gamer Inalámbricos - Amarillos</a>
+                            </div>
+                            <a href="#">
+                                <img style="height: 400px; width: 760px;" src="Media/Img/Contactanos/promos/audiff3.jpeg"/>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="image_title">
+                                <a href="#">Audífonos Gamer Inalámbricos - Verde Platino</a>
+                            </div>
+                            <a href="#">
+                                <img style="height: 400px; width: 760px;" src="Media/Img/Contactanos/promos/audi4.jpeg"/>
+                            </a>
+                        </li>
+                        <li>
+                            <div class="image_title">
+                                <a href="#">Audífonos Gamer Inalámbricos - Negros</a>
+                            </div>
+                            <a href="#">
+                                <img style="height: 400px; width: 785px;" src="Media/Img/Contactanos/promos/audif5.jpg"/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+
+            <div class="Container-Categorias">
+                <div class="Categorias-Title" style="margin-bottom: 30px;">
+                    <h2>Marcas que manejamos</h2>
+                    <p style="font-size: 20px; font-style: italic;" class="text-center">Descubre la gran cantidad de marcas que tenemos para ti</p>
+                </div>
+                <div class="Categorias" style="margin-bottom: 50px;">
                     <div class="Categoria-Foto">
                         <a href="#">
-                            <img src="Media/Img/Inicio/Diadema.jpg" class="Img-Cat">
-                            <div class="Text-Categoria">Diademas</div>
+                            <img src="Media/Img/Inicio/apple_marca2.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Apple</div>
 
                         </a>
                     </div>
                     <div class="Categoria-Foto">
                         <a href="#">
-                            <img src="Media/Img/Inicio/Earbuds.jpg" class="Img-Cat">
-                            <div class="Text-Categoria">EarBuds</div>
+                            <img src="Media/Img/Inicio/jbl_marca.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">JBL</div>
+                        </a>
+                    </div>
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/sony_marca.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Sony</div>
+                        </a>
+                    </div>
+                </div>
+                <div class="Categorias">
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/bose_marcas.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Bose</div>
+
+                        </a>
+                    </div>
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/sennheiser_marca.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Sennheiser</div>
+                        </a>
+                    </div>
+                    <div class="Categoria-Foto">
+                        <a href="#">
+                            <img src="Media/Img/Inicio/huawei_marca.jpg" class="Img-Cat">
+                            <div class="Text-Categoria">Huawei</div>
                         </a>
                     </div>
                 </div>
@@ -78,10 +162,10 @@
             <div class="Container-Ofertas">
                 <div class="container">
                     <div class="row justify-content-center">
-                        <div class="col-lg-7 text-center">
-                            <div class="section-title">
+                        <div class="col-lg-8 text-center">
+                            <div class="Categorias-Title">
                                 <h2>Ultimas Ofertas</h2>
-                                <p>Aun no se acaba el buen fin aprovecha las ofertas que te da la familia Revolt</p>
+                                <p style="font-size: 20px; font-style: italic;">Aun no se acaba el buen fin aprovecha las ofertas que te da la familia Revolt</p>
                             </div>
                         </div>
                     </div>
@@ -157,7 +241,10 @@
         </footer>
     </div>
     
-
+    <script src="js/NavMenu.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
  
 </body>
 </html>
