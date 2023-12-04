92b9a093e0f87a54a72d68a23f09618239d935a7
Emiliano-Jahir-Espinoza-Herrera10 <124842163+Emiliano-Jahir-Espinoza-Herrera10@users.noreply.github.com>
2023-12-03 20:24:07 -0600

    Add files via upload

---
+++ NUDE_PROJECT/footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos_web/estilos.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <title>Tu Página Web</title>
+</head>
+<body>
+  <center>
+<footer>
+    <div class="footer-content">
+        <div class="contact-info">
+            <h2>Información de Contacto</h2>
+            <p><i class="fas fa-map-marker-alt"></i> Dirección: Aguascalientes MX</p>
+            <p><i class="fas fa-phone"></i> Teléfono: +449-584-4979</p>
+            <p><i class="fas fa-envelope"></i> Correo Electrónico: revoltstudio@empresa.com</p>
+        </div>
+    
+        <div class="social-links">
+            <h2>Síguenos en Redes Sociales</h2>
+            <div class="icon-container">
+                <span class="separator">|</span>
+                <a href="https://www.facebook.com/emi.harrera"><i class="fab fa-facebook-f fa-lg"></i></a>
+                <span class="separator">|</span>
+                <a href="https://twitter.com/emiiherrerra_10"><i class="fab fa-twitter fa-lg"></i></a>
+                <span class="separator">|</span>
+                <a href="https://www.instagram.com/e.jherrera.10/"><i class="fab fa-instagram fa-lg"></i></a>
+                <span class="separator">|</span>
+                <a href="https://www.youtube.com"><i class="fab fa-youtube fa-lg"></i></a>
+                <span class="separator">|</span>
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="copyright">
+        &copy; 2023 REVOLT-STUDIO| Todos los derechos reservados.
+    </div>
+    <div class="empresa2">
+        <img src="Img/logo_empresa2.jpeg" width="100"  alt="">
+    </div>
+</footer>
+</center>
+</body>
+</html>
---
+++ NUDE_PROJECT/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formas de Pago</title>
+    <link rel="stylesheet" href="estilos_web/CabeceraEstilos.css">
+</head>
+<body>
+    <div class="Container-Inicio">
+        <header class="Cabecera-Header">
+            <div class="Container-Cabecera Nav-Container">
+                <img src="img/logo_empresa.jpeg" alt="LOGO" style="width: 150px;  height: 150px;">
+                <nav class="Cabecera-Nav Cabecera-Contenido">
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">Categorias</a></li>
+                        <li><a href="#">Conocenos</a></li>
+                        <li><a href="#">Acerca De</a></li>
+                    </ul>
+                </nav>
+                <nav class="Cabecera-Nav Cabecera-Sesion">
+                    <ul>
+                        <li><a href="#">Registrate</a></li>
+                        <li><a href="#">Inciar Sesion</a></li>
+                    </ul>
+                </nav>
+                <div class="Cabecera-BarraBusqueda">
+                    <form action="">
+                        <button><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i></button>
+                        <input type="text" placeholder="¿Qué Esta Buscando?">
+                    </form>
+                </div>
+                <a href="#"><i class="fa-solid fa-cart-shopping fa-2xl" style="color: #ffffff;"></i></a>
+            </div>
+        </header>
